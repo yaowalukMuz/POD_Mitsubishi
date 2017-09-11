902dfd5f74dbee5c7654928132fecda278ee6409
yaowamuz <yaowamuz.c@gmail.com>
2017-09-11 17:06:13 +0700

    set fragment page success

---
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/pod_mitsubishi/JobListDetailFragment.java
@@ -0,0 +1,29 @@
+package com.hitachi_tstv.yodpanom.yaowaluk.pod_mitsubishi;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JobListDetailFragment extends Fragment {
+
+
+    public JobListDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_job_list_detail, container, false);
+    }
+
+}
--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/pod_mitsubishi/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/pod_mitsubishi/MainActivity.java
@@ -1,20 +1,35 @@
 package com.hitachi_tstv.yodpanom.yaowaluk.pod_mitsubishi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.usernameEditText)
+    EditText usernameEditText;
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+    @BindView(R.id.signinBtn)
+    Button signinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,5 +63,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @OnClick(R.id.signinBtn)
+    public void onViewClicked() {
+        Intent i = new Intent(getApplicationContext(), JobListActivity.class);
+        i.putExtra("USERNAME", usernameEditText.getText().toString());
+        i.putExtra("PASSWORD", passwordEditText.getText().toString());
+        startActivity(i);
     }
 }
